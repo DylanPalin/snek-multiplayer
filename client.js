f0638facc842bf6d2e916e97dfdd24863759d3af
Dylan Palin <dylanpalin@gmail.com>
2023-11-22 16:12:39 -0700

    added name and idle msg

---
+++ client.js
@@ -0,0 +1,28 @@
+const net = require("net");
+
+const connect = function () {
+  const conn = net.createConnection({
+    host: "165.227.47.243",
+    port: 50541,
+  });
+  
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+    conn.on("connect", () => {
+      console.log("Successfully connected to game server");
+        conn.write("Name: DYL");
+    });
+
+    conn.on("data", () => {
+      console.log("you ded cuz you idled");
+    });
+
+
+  return conn;
+
+
+};
+
+module.exports = { connect };
--- play.js
+++ play.js
@@ -5,7 +5,15 @@ process.stdout.write('\x07');
 const { Game } = require('./src/Game')
 const { UserInterface } = require('./src/UserInterface')
 const { RemoteInterface } = require('./src/RemoteInterface')
+const { connect } = require("./client");
 const game = new Game(new UserInterface(), new RemoteInterface())
+
+// Connect to server
+
+console.log("Connecting ...");
+connect();
+
 
 // Begin game
 game.start()
+
